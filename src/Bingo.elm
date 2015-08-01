5625aba3403ca305d7e471c34ac3e54edc5696f1
Sean Omlor <somlor@eml.cc>
2015-08-01 19:36:03 -0400

    entryList with entryItems

--- src/Bingo.elm
+++ src/Bingo.elm
@@ -13,16 +13,35 @@ title message times =
     |> text
 
 pageHeader =
-  h1 [ id "title" ] [ title "bingo!" 3]
+  h1 [ id "title" ] [ title "bingo!" 3 ]
 
 pageFooter =
-  footer []
-    [ a [ href "http://seanomlor.com" ]
-        [ text "Sean Omlor" ]
+  footer [ ]
+    [
+      a [ href "http://seanomlor.com" ] [ text "Sean Omlor" ]
+    ]
+
+entryItem phrase points =
+  li [ ]
+    [
+      span [ class "phrase" ] [ text phrase ],
+      span [ class "points" ] [ text (toString points) ]
+    ]
+
+entryList =
+  ul [ ]
+    [
+      entryItem "Future-Proof" 100,
+      entryItem "Doing Agile" 200
     ]
 
 view =
-  div [ id "container" ] [ pageHeader, pageFooter ]
+  div [ id "container" ]
+    [
+      pageHeader,
+      entryList,
+      pageFooter
+    ]
 
 main =
   view
