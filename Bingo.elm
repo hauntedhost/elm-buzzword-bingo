a1c5ebb34bf5c96476dda0872495b48e30dce91b
Sean Omlor <somlor@eml.cc>
2015-08-01 15:31:53 -0400

    import with exposing

--- Bingo.elm
+++ Bingo.elm
@@ -1,14 +1,14 @@
 module Bingo where
 
-import Html
-import String
+import Html exposing (..)
+import String exposing (repeat, toUpper, trimRight)
 
 title message times =
   message ++ " "
-    |> String.toUpper
-    |> String.repeat times
-    |> String.trimRight
-    |> Html.text
+    |> toUpper
+    |> repeat times
+    |> trimRight
+    |> text
 
 main =
   title "bingo!" 3
