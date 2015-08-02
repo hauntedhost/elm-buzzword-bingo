70c4ffdd16dc33ff03355580e8c9ab1f96bc8541
Sean Omlor <somlor@eml.cc>
2015-08-01 20:38:38 -0400

    gulp build error handling

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,21 @@
 var gulp = require('gulp');
+var gutil = require('gulp-util');
+var plumber = require('gulp-plumber');
 var elm  = require('gulp-elm');
+
+// build error handler
+var onError = function(error) {
+  gutil.beep();
+  gutil.log(error.toString());
+};
 
 // elm build
 gulp.task('elm-init', elm.init);
 gulp.task('elm', ['elm-init'], function(){
   return gulp.src('src/*.elm')
+    .pipe(plumber({
+      errorHandler: onError
+    }))
     .pipe(elm())
     .pipe(gulp.dest('build/'))
 });
